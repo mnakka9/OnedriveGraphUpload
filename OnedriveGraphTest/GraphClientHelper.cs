3c3b3c31789ee65946e1d5adb1b58f6ac7349421
Mahesh Nakka <Mahesh_Nakka@epam.com>
2018-03-03 17:54:05 +0300

    Add project files.

---
+++ OnedriveGraphTest/GraphClientHelper.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+using System.Net.Http.Headers;
+using OnedriveGraphTest;
+using System.Diagnostics;
+
+namespace OnedriveGraphTest
+{
+    public static class GraphClientHelper
+    {
+        private static string ClientId = "7c70606c-c585-4850-aa15-5717ee1227b9";
+        private static String[] Scopes = {
+                        "User.Read",
+                        "User.ReadBasic.All",
+                        "Mail.Send",
+                        "Mail.Read",
+                        "Group.ReadWrite.All",
+                        "Sites.Read.All",
+                        "Directory.AccessAsUser.All",
+                        "Files.ReadWrite",
+                        "Files.ReadWrite.AppFolder"
+                        };
+        public static PublicClientApplication IdentityClientApp = new PublicClientApplication(ClientId);
+
+        public static string TokenForUser = null;
+        public static DateTimeOffset Expiration;
+
+        private static GraphServiceClient graphClient = null;
+
+        // Get an access token for the given context and resourceId. An attempt is first made to 
+        // acquire the token silently. If that fails, then we try to acquire the token by prompting the user.
+        public static GraphServiceClient GetAuthenticatedClient()
+        {
+            if (graphClient == null)
+            {
+                // Create Microsoft Graph client.
+                try
+                {
+                    graphClient = new GraphServiceClient(
+                        "https://graph.microsoft.com/v1.0",
+                        new DelegateAuthenticationProvider(
+                            async (requestMessage) =>
+                            {
+                                var token = await GetTokenForUserAsync(IdentityClientApp);
+                                requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", token);
+
+                            }));
+                    return graphClient;
+                }
+
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("Could not create a graph client: " + ex.Message);
+                }
+            }
+
+            return graphClient;
+        }
+
+
+        /// <summary>
+        /// Get Token for User.
+        /// </summary>
+        /// <returns>Token for user.</returns>
+        public static async Task<string> GetTokenForUserAsync(PublicClientApplication clientApp)
+        {
+            AuthenticationResult authResult;
+            try
+            {
+                if (clientApp.Users.Count() > 0)
+                {
+                    authResult = await IdentityClientApp.AcquireTokenSilentAsync(Scopes,clientApp.Users.FirstOrDefault());
+                    TokenForUser = authResult.AccessToken;
+                }
+                else
+                {
+                    throw new Exception("Get correct access token");
+                }
+                
+            }
+
+            catch (Exception)
+            {
+                if (TokenForUser == null || Expiration <= DateTimeOffset.UtcNow.AddMinutes(5))
+                {
+                    authResult = await IdentityClientApp.AcquireTokenAsync(Scopes);
+
+                    TokenForUser = authResult.AccessToken;
+                    Expiration = authResult.ExpiresOn;
+                }
+            }
+
+            return TokenForUser;
+        }
+
+        /// <summary>
+        /// Signs the user out of the service.
+        /// </summary>
+        public static void SignOut()
+        {
+            graphClient = null;
+            TokenForUser = null;
+
+        }
+    }
+
+}
---
+++ OnedriveGraphTest/MainWindow.xaml.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using ByteSizeLib;
+using MetroFramework;
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
+
+namespace OnedriveGraphTest
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private string[] selectedFiles = new string[0];
+		Form formForMessage = new Form();
+		private StringBuilder errorMessages = new StringBuilder();
+		GraphServiceClient graphClient = null;
+		public MainWindow()
+		{
+			InitializeComponent();
+			graphClient = GraphClientHelper.GetAuthenticatedClient();
+		}
+
+		private void filesbtn_Click(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog fileDialog = new OpenFileDialog();
+			fileDialog.Filter = "*.epub | *.*";
+			fileDialog.InitialDirectory = "D:";
+			fileDialog.Multiselect = true;
+			if (fileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				selectedFiles = fileDialog.FileNames;
+			}
+
+			if (selectedFiles != null && selectedFiles.Count() > 0)
+			{
+				List<CustomeName> lstItems = new List<CustomeName>();
+				var fileInfo = new FileInfo(selectedFiles[0]);
+				dirName.Content = $"Directory : {fileInfo.DirectoryName}";
+				foreach (var file in selectedFiles)
+				{
+					lstItems.Add(new CustomeName() { Name = (new FileInfo(file)).Name });
+				}
+
+				lstView1.ItemsSource = lstItems;
+			}
+
+		}
+
+		private async void btnUpload_Click(object sender, RoutedEventArgs e)
+		{
+			if (selectedFiles == null || selectedFiles.Count() == 0)
+			{
+				System.Windows.MessageBox.Show("Please select atleast one file to upload!","Stop!",MessageBoxButton.OK,MessageBoxImage.Warning);
+				return;
+			}
+
+			try
+			{
+				spinner.Visibility = Visibility.Visible;
+				spinner.Spin = true;
+				btnUpload.IsEnabled = false;
+				filesbtn.IsEnabled = false;
+				if (graphClient == null)
+				{
+					graphClient = GraphClientHelper.GetAuthenticatedClient();
+				}
+				var count = 100 / selectedFiles.Count();
+				if (System.IO.File.Exists("log.txt"))
+				{
+					System.IO.File.Delete("log.txt");
+				}
+				foreach (var file in selectedFiles)
+				{
+					var fileName = Path.GetFileName(file);
+					try
+					{
+						if (file != null && file.Contains("."))
+						{
+
+							await UploadFilesToOneDrive(fileName, file, graphClient);
+							progressBar.Value += count;
+						}
+					}
+					catch (Exception ex)
+					{
+						errorMessages.AppendLine($"File: {fileName} upload failed:");
+						errorMessages.AppendLine($"Message :{ ex.Message }");
+						errorMessages.AppendLine($"{ ex.StackTrace }");
+						System.IO.File.AppendAllText("log.txt", errorMessages.ToString());
+						System.Windows.MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+						continue;
+					}
+				}
+				if (!System.IO.File.Exists("log.txt"))
+				{
+					System.Windows.MessageBox.Show("Successfully uploaded");
+				}
+			}
+			catch (Exception ex)
+			{
+				spinner.Spin = false;
+				spinner.Visibility = Visibility.Hidden;
+				errorMessages.AppendLine($"Message :{ ex.Message }");
+				errorMessages.AppendLine($"{ ex.StackTrace }");
+				System.IO.File.AppendAllText("log.txt", errorMessages.ToString());
+				System.Windows.MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+			finally
+			{
+				dirName.Content = "Directory: ";
+				lstView1.ItemsSource = null;
+				selectedFiles = new string[0];
+				btnUpload.IsEnabled = true;
+				filesbtn.IsEnabled = true;
+				spinner.Spin = false;
+				spinner.Visibility = Visibility.Hidden;
+				progressBar.Value = 0;
+				if (System.IO.File.Exists("log.txt"))
+				{
+					var result = Process.Start("log.txt");
+					Thread.Sleep(5000);
+					if (result.HasExited)
+					{
+						System.IO.File.Delete("log.txt");
+					}
+				}
+			}
+		}
+
+
+		/// <summary>
+		/// UploadFiles to Onedrive Less than 4MB only
+		/// </summary>
+		/// <param name="fileName"></param>
+		/// <param name="filePath"></param>
+		/// <param name="graphClient"></param>
+		/// <returns></returns>
+		private static async Task UploadFilesToOneDrive(string fileName, string filePath, GraphServiceClient graphClient)
+		{
+			try
+			{
+				var uploadPath = $"/CodeUploads/{DateTime.Now.ToString("ddMMyyyy")}/" + Uri.EscapeUriString(fileName);
+
+
+				using (var stream = new FileStream(filePath, FileMode.Open))
+				{
+					if (stream != null)
+					{
+						var fileSize = ByteSize.FromBytes(stream.Length);
+						if (fileSize.MegaBytes > 4)
+						{
+							var session = await graphClient.Drive.Root.ItemWithPath(uploadPath).CreateUploadSession().Request().PostAsync();
+							var maxSizeChunk = 320 * 4 * 1024;
+							var provider = new ChunkedUploadProvider(session, graphClient, stream, maxSizeChunk);
+							var chunckRequests = provider.GetUploadChunkRequests();
+							var exceptions = new List<Exception>();
+							var readBuffer = new byte[maxSizeChunk];
+							DriveItem itemResult = null;
+							//upload the chunks
+							foreach (var request in chunckRequests)
+							{
+								// Do your updates here: update progress bar, etc.
+								// ...
+								// Send chunk request
+								var result = await provider.GetChunkRequestResponseAsync(request, readBuffer, exceptions);
+
+								if (result.UploadSucceeded)
+								{
+									itemResult = result.ItemResponse;
+								}
+							}
+
+							// Check that upload succeeded
+							if (itemResult == null)
+							{
+								await UploadFilesToOneDrive(fileName, filePath, graphClient);
+							}
+						}
+						else
+						{
+							await graphClient.Drive.Root.ItemWithPath(uploadPath).Content.Request().PutAsync<DriveItem>(stream);
+						}
+					}
+				}
+			}
+			catch
+			{
+				throw;
+			}
+		}
+
+		protected override void OnClosed(EventArgs e)
+		{
+			formForMessage = null;
+			base.OnClosed(e);
+		}
+
+	}
+
+	public class CustomeName
+	{
+		public string Name { get; set; }
+	}
+}
